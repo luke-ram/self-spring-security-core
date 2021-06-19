0efa461d00d2656687a975a0f8584b3f5604f077
luke-ram <ramhosoo@gmail.com>
2021-06-20 01:32:04 +0900

    AjaxAccessDeniedHandler, AjaxLoginUrlAuthenticationEntryPoint 완료

---
+++ src/main/java/io/security/corespringsecurity/security/common/AjaxLoginAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package io.security.corespringsecurity.security.common;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component
+public class AjaxLoginAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,"UnAuthorized");
+    }
+}
